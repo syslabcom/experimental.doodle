0659634b1626143a737b996ea920f6a285e6a85c
ale-rt <alessandro.pisa@gmail.com>
2026-05-20 10:46:50 +0200

    Add a basic folderish contenttype

--- src/experimental/doodle/interfaces.py
+++ src/experimental/doodle/interfaces.py
@@ -1,7 +1,26 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from plone.autoform import directives as form
+from plone.supermodel import model
+from zope import schema
 
 
 class IBrowserLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+# MVP: Folderish Doodle container schema interface
+class IExperimentalDoodleFolder(model.Schema):
+    """Folderish Doodle container (MVP)"""
+    # Dublin Core fields (title, description) are included by default
+    form.order_after(description='title')
+    description = schema.Text(
+        title="Description",
+        required=False,
+    )
+    # Optionally, add a text field for body/content
+    body = schema.Text(
+        title="Body",
+        required=False,
+    )
