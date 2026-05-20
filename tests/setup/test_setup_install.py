4be89b52909ce4ee5ffadee4d9017a12006dfabb
ale-rt <alessandro.pisa@gmail.com>
2026-05-20 10:57:00 +0200

    Register the contentype

--- tests/setup/test_setup_install.py
+++ tests/setup/test_setup_install.py
@@ -15,3 +15,7 @@ class TestSetupInstall:
     def test_latest_version(self, profile_last_version):
         """Test latest version of default profile."""
         assert profile_last_version(f"{PACKAGE_NAME}:default") == "1000"
+
+    def test_folder_type_installed(self, portal):
+        """Test folder content type is registered."""
+        assert "experimental.doodle.folder" in portal.portal_types
